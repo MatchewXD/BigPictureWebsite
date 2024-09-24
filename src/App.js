208960c2d94d0b29407d4c2c4c51150b9804574c
Matthew Seagren <mattseagren@gmail.com>
2024-09-23 18:55:40 -0700

    Set up a react app and started building the structure of the website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import MyRoutes from "./components/MyRoutes";
 
 function App() {
   return (
     <div className="App">
+      <MyRoutes />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
