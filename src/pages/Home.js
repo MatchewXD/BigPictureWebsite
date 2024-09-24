fd0f6275fdfde8b5e8cf114a77244e94c227a453
Matthew Seagren <mattseagren@gmail.com>
2024-09-23 19:08:27 -0700

    Cleaned up the default react app html

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <header>
+        <h1>Welcome to My React App</h1>
+        <p>This is a simple home page example.</p>
+        <img
+          src="logo192.png"
+          alt="React Logo"
+          style={{ height: "150px", animation: "spin 20s linear infinite" }}
+        />
+      </header>
+    </div>
+  );
+};
+
+export default Home;
